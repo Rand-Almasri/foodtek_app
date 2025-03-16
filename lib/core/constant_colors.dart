c2118f746a0eee327a8c77eb64f29f99dc3b29c1
sarahkronz <145930898+sarahkronz@users.noreply.github.com>
2025-03-16 03:41:29 +0300

    Implemented splash screen

---
+++ lib/core/constant_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryGreen = Color(0xFF25AE4B);
+  static const Color secondary = Color(0xFF455A64);
+  static const Color background = Colors.white;
+
+
+
+}
