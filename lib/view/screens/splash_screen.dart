8fead526b614e2118f4c4854db0504591aa2a959
sarahkronz <145930898+sarahkronz@users.noreply.github.com>
2025-03-18 14:16:54 +0300

    delete onbarding screens and modify splash screen time

--- lib/view/screens/splash_screen.dart
+++ lib/view/screens/splash_screen.dart
@@ -15,7 +15,7 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
 
-    Future.delayed(const Duration(seconds: 10), () {
+    Future.delayed(const Duration(seconds: 5), () {
       Navigator.pushReplacementNamed(context, AppRoutes.onboarding1);
     });
   }
